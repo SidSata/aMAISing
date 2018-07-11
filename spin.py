5a0b96425cfa56130f93a8cb328b41a0e524d648
Parth <parth.behani@gmail.com>
2018-07-11 10:57:11 +0530

    spin

---
+++ spin.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+#right wheel
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+#left wheel
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
+
+GPIO.output(20, GPIO.HIGH)
+GPIO.output(21, GPIO.LOW)
+GPIO.output(26, GPIO.HIGH)
+GPIO.output(19, GPIO.LOW)
+ 
+time.sleep(1000)
+GPIO.cleanup()
