94102c0d762bef7859d3e8383e985fb031d4ee56
Parth mac <parth.behani@gmail.com>
2020-01-12 03:22:17 +0530

    lcd + face recog

---
+++ testIOpins/testIOpins.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
+  pinMode(1,OUTPUT);
+
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(2, HIGH);
+  digitalWrite(1, LOW);
+
+}
