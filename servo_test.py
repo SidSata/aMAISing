fe6dedb356e477bc354e46a580cee54faaf8cc35
SidSata <59756688+SidSata@users.noreply.github.com>
2020-01-19 20:46:54 +0530

    Commented servo.py

--- servo_test.py
+++ servo_test.py
@@ -1,12 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 
+# testing servo motor when connected to Raspberry Pi
+
 servoPIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+# GPIO 17 for PWM with 50Hz
+p = GPIO.PWM(servoPIN, 50)
+
+# Initialization
+p.start(2.5) 
 try:
   while True:
     p.ChangeDutyCycle(2.5)
