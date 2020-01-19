04cb23e3f89945c8206e2b621e7fb17f2014924e
SidSata <59756688+SidSata@users.noreply.github.com>
2020-01-19 20:12:27 +0530

    dctest comments

--- dctest/dctest.ino
+++ dctest/dctest.ino
@@ -1,12 +1,16 @@
+
+
+//tests the two dc motors
+
 void setup() {
-  // put your setup code here, to run once:
+  //dc motors connected to pins 8 and 9
 pinMode(9, OUTPUT);
 pinMode(8, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //starts the motor, then stops it after a second's delay to test it
 digitalWrite(9, HIGH);
 digitalWrite(8, HIGH);
 delay(1000);
